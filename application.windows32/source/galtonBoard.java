58a81057e358b9cd82ca79ea102e478c797b1f96
hyprox1413 <115951029+hyprox1413@users.noreply.github.com>
2022-10-16 22:27:12 -0400

    added files

---
+++ application.windows32/source/galtonBoard.java
@@ -0,0 +1,214 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class galtonBoard extends PApplet {
+
+class Marble {
+  float x, y, dx, dy, r, theta, h;
+  boolean collided, collidedCheck;
+
+  Marble() {
+    x = width / 2 + random(20) - 10;
+    y = 100;
+    dx = 0;
+    dy = 0;
+    r = 5;
+    h = random(255);
+  }
+
+  public void setTheta() {
+    if (dy > 0) {
+      theta = acos(dx / dist(0, 0, dx, dy)) + PI;
+    } else if (dy < 0) {
+      theta = -acos(dx / dist(0, 0, dx, dy)) + PI;
+    } else if (dx < 0) {
+      theta = 0;
+    } else {
+      theta = PI;
+    }
+    theta += TAU;
+    theta %= TAU;
+  }
+
+  public void collide() {
+    float pTheta;
+    collidedCheck = false;
+    for (Post p : posts) {
+      if (!collided) {
+        if (dist(x, y, p.x, p.y) < r + p.r) {
+          println(1);
+          collided = true;
+          collidedCheck = true;
+          if (y < p.y) {
+            pTheta = acos((p.x - x) / dist(x, y, p.x, p.y)) + PI;
+          } else if (y > p.y) {
+            break;
+          } else if (x > p.x) {
+            pTheta = 0;
+          } else {
+            pTheta = PI;
+          }
+          pTheta += TAU;
+          pTheta %= TAU;
+          setTheta();
+          theta = pTheta + (pTheta - theta) + random(0.01f) - 0.005f;
+          theta += TAU;
+          theta %= TAU;
+          dx = cos(theta) * dist(0, 0, dx, dy) / 1.8f;
+          dy = sin(theta) * dist(0, 0, dx, dy) / 1.8f;
+        }
+      }
+    }
+
+    if (collidedCheck) {
+      collided = true;
+    } else {
+      collided = false;
+    }
+  }
+
+
+  public void update() {
+    collide();
+    x += dx;
+    y += dy;
+    dy += 0.2f;
+  }
+
+  public void render() {
+    stroke(0);
+    if (y > 600) {
+      noStroke();
+    }
+    colorMode(HSB, 255);
+    stroke(h, 255, 255);
+    fill(h, 255, 255, 255);
+    ellipse(x, y, 2 * r, 2 * r);
+    colorMode(RGB, 255);
+  }
+}
+
+
+class Post {
+  float x, y, r;
+
+  Post(float x, float y) {
+    this.x = x;
+    this.y = y;
+    r = 5;
+  }
+
+  public void render() {
+    fill(0);
+    stroke(0);
+    ellipse(x, y, 2 * r, 2 * r);
+  }
+}
+
+public void fillPosts() {
+  columns = 21;
+  xs = 25;
+  float ys = xs * sin(PI / 3);
+  for (float y = 1; y <= columns; y ++) {
+    for (float x = 0; x < y + 10; x ++) {
+      Post post = new Post(width / 2 + (x - y / 2 - 5) * xs, 100 + y * ys);
+      posts.add(post);
+    }
+  }
+}
+
+public void fillMarbles() {
+  for (int i = 0; i < 1000; i ++) {
+    Marble marble = new Marble();
+    marbles.add(marble);
+  }
+}
+
+class Bar {
+  float x, h;
+  int v = 0;
+
+  Bar(float x) {
+    this.x = x;
+    h = 0;
+  }
+
+  public void render() {
+    if (max != 0) {
+      h = (float) 480 / max * v;
+    }
+    fill(0);
+    rect(x, height - h, 1280 / barCt, h);
+  }
+}
+
+public void fillBars() {
+  for (int i = 0; i < barCt; i ++) {
+    Bar bar = new Bar(width / 2 - 640 + i * (1280 / barCt));
+    bars.add(bar);
+  }
+}
+
+ArrayList<Post> posts;
+ArrayList<Marble> marbles;
+ArrayList<Bar> bars;
+int columns;
+int barCt = 50;
+float xs;
+int max = 0;
+
+public void setup() {
+  
+  posts = new ArrayList<Post>();
+  fillPosts();
+  marbles = new ArrayList<Marble>();
+  fillMarbles();
+  bars = new ArrayList<Bar>();
+  fillBars();
+}
+
+public void draw() {
+  if (millis() > 10000) {
+    background(255);
+    for (int i = 0; i < marbles.size(); i ++) {
+      Marble m = marbles.get(i);
+      m.update();
+      m.render();
+      if (m.y > 600) {
+        bars.get((int) (m.x - 320) / (1280 / barCt)).v ++;
+        marbles.remove(i);
+        i --;
+      }
+    }
+    for (Post p : posts) {
+      p.render();
+    }
+    for (Bar b : bars) {
+      if (b.v > max) {
+        max = b.v;
+      }
+      b.render();
+    }
+  }
+}
+  public void settings() {  fullScreen(P2D); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "galtonBoard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
